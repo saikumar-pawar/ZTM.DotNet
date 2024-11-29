6032012ac6040913cba2d7fa7df3e0b9cff355a4
“saikumar” <pawarsaikumar82@gmail.com>
2024-11-29 23:33:07 +0530

    String Parsing

---
+++ StringParsing/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+string luckyNumber = "12";
+int parsedLuckyNumber = int.Parse(luckyNumber);
+
+double temperature = double.Parse("32.4");
+
+CultureInfo cultureInfo = new CultureInfo("de-DE");
+temperature = double.Parse("28.6", cultureInfo);
+
+temperature = double.Parse("28,6", cultureInfo);
+Console.ReadKey();
