27b64399365e159f990748f3002b345e94dd64e4
“saikumar” <pawarsaikumar82@gmail.com>
2024-11-29 23:52:45 +0530

    Date / Time

---
+++ DateTime/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+DateTime dateOfBirth = new DateTime(1995, 1, 20);
+Console.WriteLine(dateOfBirth);
+Console.WriteLine(dateOfBirth.DayOfWeek);
+
+//Computed Values
+DateTime today = DateTime.Today;
+DateTime now = DateTime.Now;
+DateTime utcNow = DateTime.UtcNow;
+
+//DateTime Parsing
+DateTime localDate = DateTime.Parse("29-11-2024");
+DateTime usDate = DateTime.Parse("11/29/2024", new CultureInfo("en-US"));
+
+Console.WriteLine($"localDate: {localDate}\tusDate: {usDate}");
+
+//DateTime Formatting
+Console.WriteLine(dateOfBirth.ToString());
+Console.WriteLine(dateOfBirth.ToString(new CultureInfo("en-US")));
+Console.WriteLine(dateOfBirth.ToString("yyyy-MM-dd"));
