93d5fbbb9adb7c3f3672926ea21559ed28c9b388
“saikumar” <pawarsaikumar82@gmail.com>
2024-11-30 17:24:15 +0530

    Iteration Statements

---
+++ IterationStatements/Program.cs
@@ -0,0 +1,24 @@
+﻿//while
+int a = 1;
+
+while (a < 5)
+{
+    Console.WriteLine($"while: {a}");
+    a++;
+}
+
+//do-while
+int b = 1;
+
+do
+{
+    Console.WriteLine($"do-while: {b}");
+    b++;
+}
+while (b < 3);
+
+//for
+for (int i = 1; i < 3; i++)
+{
+    Console.WriteLine($"for: {i}");
+}
