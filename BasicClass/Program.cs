480a56c73171ceb1607483fc48c26fdd71f25d70
“saikumar” <pawarsaikumar82@gmail.com>
2024-12-01 01:55:53 +0530

    Basic Class

---
+++ BasicClass/Program.cs
@@ -0,0 +1,40 @@
+﻿Person person1 = new Person("Jason");
+Person person2 = new Person("Jack");
+
+Console.WriteLine(person1.Name);
+Console.WriteLine(person2.Name);
+person1.PrintGreeting();
+person2.PrintGreeting();
+string greeting = person1.GetGreeting();
+Console.WriteLine(greeting);
+
+Math m = new Math();
+Console.WriteLine(m.Sum(32, 46));
+
+class Person
+{
+    public Person(string name)
+    {
+        Name = name;
+    }
+
+    public string Name { get; private set; }
+
+    public void PrintGreeting()
+    {
+        Console.WriteLine($"Hello, {Name}!");
+    }
+
+    public string GetGreeting()
+    {
+        return $"Hello, {Name}!";
+    }
+}
+
+class Math
+{
+    public long Sum(int a, int b)
+    {
+        return (long)a + b;
+    }
+}
