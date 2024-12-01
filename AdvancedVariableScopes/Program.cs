488cbfad1bf8771da719aa079f8a64cce8d8596e
“saikumar” <pawarsaikumar82@gmail.com>
2024-12-01 15:44:10 +0530

    Advanced Variable Scopes

---
+++ AdvancedVariableScopes/Program.cs
@@ -0,0 +1,28 @@
+﻿
+Book book = new Book("grokking algorithms", "Aditya Y. Bhargava");
+book.PrintSummary();
+Console.WriteLine(book.GetTitleLengthWithoutSpaces());
+
+public class Book
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+
+    public Book(string title, string author)
+    {
+        Title = title;
+        Author = author;
+    }
+
+    public void PrintSummary()
+    {
+        string summary = $"{Title} - {Author}";
+        Console.WriteLine(summary);
+    }
+
+    public int GetTitleLengthWithoutSpaces()
+    {
+        string titleWithoutSpaces = Title.Replace(" ", "");
+        return titleWithoutSpaces.Length;
+    }
+}
