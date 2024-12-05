b483587bf7630a078643bf837962521638fd6ccd
“saikumar” <pawarsaikumar82@gmail.com>
2024-12-06 01:00:57 +0530

    Passing Reference-Type Parameters

--- MethodArguments/Program.cs
+++ MethodArguments/Program.cs
@@ -1,4 +1,6 @@
-﻿Rectangle rectangle = new Rectangle(600, 400);
+﻿//value types
+
+Rectangle rectangle = new Rectangle(600, 400);
 Console.WriteLine($"rectangle height: {rectangle.Height}");
 
 MethodParameters.ChangeHeight( rectangle );
@@ -9,6 +11,26 @@ Console.WriteLine($"rectangle width: {rectangle2.Width}");
 
 MethodParameters.ChangeWidth( ref rectangle2 );
 Console.WriteLine($"rectangle width: {rectangle2.Width}");
+
+//reference types
+
+Person person = new Person("Rajesh");
+Console.WriteLine($"Name: {person.Name}");
+
+MethodParameters.ChangeName( person );
+Console.WriteLine($"Name: {person.Name}");
+
+Person person2 = new Person("Sam");
+Console.WriteLine($"Name: {person2.Name}");
+
+MethodParameters.ChangeNameByReference( ref person2 );
+Console.WriteLine($"Name: {person2.Name}");
+
+Person person3 = new Person("Sam");
+Console.WriteLine($"Name: {person3.Name}");
+
+MethodParameters.ChangeNameByReferenceActualWorking( ref person3 );
+Console.WriteLine($"Name: {person3.Name}");
 
 public struct Rectangle
 {
@@ -34,4 +56,29 @@ public class MethodParameters
         rectangle.Width = 1200;
         Console.WriteLine($"rectangle width in method: {rectangle.Width}");
     }
+
+    public static void ChangeName(Person person)
+    {
+        person.Name = "Unknown";
+    }
+
+    public static void ChangeNameByReference(ref Person person)
+    {
+        person.Name = "Unknown";
+    }
+
+    public static void ChangeNameByReferenceActualWorking(ref Person person)
+    {
+        person.Name = "Unknown";
+        person = new Person("Ramesh");
+    }
+}
+
+public class Person
+{
+    public Person(string name)
+    {
+        Name = name;
+    }
+    public string Name { get; set; }
 }
