17b11c7ddb870f7dab35fdef4e3f26bec098fa6e
“saikumar” <pawarsaikumar82@gmail.com>
2024-12-06 00:23:56 +0530

    Structs

---
+++ Structs/Program.cs
@@ -0,0 +1,25 @@
+﻿Rectangle rectangle = new Rectangle(3, 9);
+ImmutableRectangle immutableRectangle = new ImmutableRectangle(6, 12);
+//immutableRectangle.Height = 8;
+
+public struct Rectangle
+{
+    public Rectangle(double height, double width)
+    {
+        Height = height;
+        Width = width;
+    }
+    public double Height { get; set; }
+    public double Width { get; set; }
+}
+
+public readonly struct ImmutableRectangle
+{
+    public ImmutableRectangle(double height, double width)
+    {
+        Height = height;
+        Width = width;
+    }
+    public double Height { get; init; }
+    public double Width { get; init; }
+}
