e7ca65b467610a2c82f06f53b7e4dae663285ca3
“saikumar” <pawarsaikumar82@gmail.com>
2024-12-06 16:35:46 +0530

    Stack

---
+++ Stack/Program.cs
@@ -0,0 +1,16 @@
+﻿var countries = new Stack<string>();
+
+countries.Push("Thailand");
+countries.Push("China");
+countries.Push("Japan");
+
+var lastCountry = countries.Pop();
+Console.WriteLine($"The last visited country was: {lastCountry}");
+
+var previouslyVisitedCountry = countries.Peek();
+Console.WriteLine($"The previously visited country was: {previouslyVisitedCountry}");
+
+foreach (var country in countries)
+{
+    Console.WriteLine(country);
+}
