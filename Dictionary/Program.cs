40f55a8231c3c2d704200eb019f0cdaee3f076a5
“saikumar” <pawarsaikumar82@gmail.com>
2024-12-06 20:27:20 +0530

    Dictionary

---
+++ Dictionary/Program.cs
@@ -0,0 +1,26 @@
+﻿var numbers = new Dictionary<int, string>
+{
+    { 1, "one" },
+    { 2, "two" }
+};
+
+numbers.Add(3, "three");
+numbers.Add(4, "four");
+numbers[5] = "five";
+
+foreach (var number in numbers)
+{
+    Console.WriteLine($"{number.Key}: {number.Value}");
+}
+
+foreach (var key in numbers.Keys)
+{
+    Console.WriteLine(key);
+}
+
+foreach (var val in numbers.Values)
+{
+    Console.WriteLine(val);
+}
+
+Console.WriteLine(numbers.ContainsKey(9));
