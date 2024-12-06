efab5971aad77871f186076c2125b18f1d627653
“saikumar” <pawarsaikumar82@gmail.com>
2024-12-06 16:42:43 +0530

    Queue

---
+++ Queue/Program.cs
@@ -0,0 +1,11 @@
+﻿var messages = new Queue<string>();
+
+messages.Enqueue("Hello");
+messages.Enqueue("Good morning");
+messages.Enqueue("Good evening");
+
+while (messages.Count > 0)
+{
+    var message = messages.Dequeue();
+    Console.WriteLine(message);
+}
