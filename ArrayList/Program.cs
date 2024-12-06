8530479fc0b3608be0a1ef539626dfb089495c85
“saikumar” <pawarsaikumar82@gmail.com>
2024-12-06 16:01:47 +0530

    ArrayList

---
+++ ArrayList/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+
+ArrayList arrayList = new ArrayList();
+
+int day = 12;
+arrayList.Add(day);
+
+Person person1 = new Person("Ram");
+arrayList.Add(person1);
+
+foreach (var item in arrayList)
+{
+    if (item is Person)
+    {
+        var person = item as Person;
+        if (person != null)
+        {
+            Console.WriteLine(person.Name);
+        }
+    }
+    else
+    {
+        Console.WriteLine(item);
+    }
+}
+
+public class Person
+{
+    public Person(string name)
+    {
+        Name = name;
+    }
+
+    public string Name { get; set; }
+}
