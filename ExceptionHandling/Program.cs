270f1385b316a7c3be7e03ef49604d0ff412125c
“saikumar” <pawarsaikumar82@gmail.com>
2024-12-06 14:37:32 +0530

    Exception Handling

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,28 @@
+﻿var content = string.Empty;
+
+try
+{
+    content = File.ReadAllText("doc.txt");
+    Console.WriteLine("The file has been read.");
+
+}
+catch (FileNotFoundException exception)
+{
+    Console.WriteLine($"ERROR: The File '{exception.FileName}' could not be found");
+}
+finally
+{
+    Console.WriteLine($"The content length is: {content.Length}");
+}
+
+var fileName = "doc.txt";
+
+if (File.Exists(fileName))
+{
+    var fileContent = File.ReadAllText(fileName);
+    Console.WriteLine(fileContent);
+}
+else
+{
+    Console.WriteLine("The file does not exist. Do you want to create it?");
+}
