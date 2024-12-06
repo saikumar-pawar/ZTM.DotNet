e5299d5417044813fa695b1e1dc87f6804018f92
“saikumar” <pawarsaikumar82@gmail.com>
2024-12-06 16:24:13 +0530

    List

---
+++ List/Program.cs
@@ -0,0 +1,28 @@
+﻿var numbers = new List<int>();
+numbers.Add(4);
+numbers.Add(8);
+
+foreach (var number in numbers)
+{
+    Console.WriteLine(number);
+}
+
+var persons = new List<Person>();
+persons.Add(new Person("Rohit"));
+persons.Add(new Person("Krishna"));
+
+foreach (var person in persons)
+{
+    Console.WriteLine(person.Name);
+}
+
+Console.WriteLine(persons[0].Name);
+
+public class Person
+{
+    public Person(string name)
+    {
+        Name = name;
+    }
+    public string Name { get; set; }
+}
