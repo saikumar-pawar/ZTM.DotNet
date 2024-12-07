3f6c2d3488726bfc0c60c256b7dcb947c45c531a
“saikumar” <pawarsaikumar82@gmail.com>
2024-12-07 15:33:43 +0530

    ParamsKeyword

---
+++ ParamsKeyword/Program.cs
@@ -0,0 +1,14 @@
+﻿var numberPrinter = new NumberPrinter();
+numberPrinter.PrintNumbers(new int[] { 5, 10, 15, 20 });
+numberPrinter.PrintNumbers(2, 4, 6, 8);
+
+public class NumberPrinter
+{
+    public void PrintNumbers(params int[] numbers)
+    {
+        foreach (var number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
