9b502045d8982ece7699932dc522f57e0f81bb5d
“saikumar” <pawarsaikumar82@gmail.com>
2024-12-07 21:15:52 +0530

    CustomExceptionTypes

---
+++ CustomExceptionTypes/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace CustomExceptionTypes;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter Username: ");
+        var userInput = Console.ReadLine();
+
+        try
+        {
+            var userName = GetNormalizedUserName(userInput);
+            Console.WriteLine($"Username: {userName}");
+        }
+        catch (InvalidUserNameException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    public static string GetNormalizedUserName(string? userName)
+    {
+        if (string.IsNullOrWhiteSpace(userName) || userName.Length < 6)
+        {
+            throw new InvalidUserNameException(userName);
+        }
+
+        return userName.ToLower();
+    }
+}
