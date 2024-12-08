714ffa7582260df5d99c998abfe8e5024f971770
“saikumar” <pawarsaikumar82@gmail.com>
2024-12-08 20:54:12 +0530

    UnitTests

---
+++ UnitTests/Test1.cs
@@ -0,0 +1,12 @@
+﻿namespace UnitTests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Console.WriteLine("Hello Test");
+        }
+    }
+}
