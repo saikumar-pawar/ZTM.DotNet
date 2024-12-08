66df773bf5ea8215e45904eca99b802f0c7802e5
“saikumar” <pawarsaikumar82@gmail.com>
2024-12-08 19:01:17 +0530

    LinqExercise

---
+++ LinqExercise/Program.cs
@@ -0,0 +1,11 @@
+﻿var buildings = new List<Building>{
+    new Building("Magadha", 1500, 200.6m),
+    new Building("Spring", 2600, 435.77m),
+    new Building("Attari", 1400, 148.28m),
+    new Building("EMpire State", 12550, 840.32m),
+};
+
+var totalEstimatedTax = buildings.Sum(b => b.SizeInSqrFt * b.TaxPerSqrFt);
+Console.WriteLine($"Total Estimated Tax: {totalEstimatedTax}");
+
+public record Building(string Name, ushort SizeInSqrFt, decimal TaxPerSqrFt);
