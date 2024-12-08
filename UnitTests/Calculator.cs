c5d4cc17b61b5396c205ea5e1644b399f3a0f88b
“saikumar” <pawarsaikumar82@gmail.com>
2024-12-08 21:18:19 +0530

    Initial Calculator and CalculatorTests class

---
+++ UnitTests/Calculator.cs
@@ -0,0 +1,14 @@
+﻿namespace UnitTests;
+
+public class Calculator
+{
+    public decimal Sum(decimal va1, decimal va2)
+    {
+        return va1 + va2;
+    }
+
+    public decimal Divide(decimal va1, decimal va2)
+    {
+        return va1 / va2;
+    }
+}
---
+++ UnitTests/CalculatorTests.cs
@@ -0,0 +1,37 @@
+﻿namespace UnitTests;
+
+[TestClass]
+public sealed class CalculatorTests
+{
+    [TestMethod]
+    public void Calculator_Sum_Return20For12And8()
+    {
+        //Arrange
+        var calculator = new Calculator();
+
+        //Act
+        var result = calculator.Sum(12, 8);
+
+        //Assert
+        Assert.AreEqual(20, result);
+    }
+
+    [TestMethod]
+    public void Calculator_Divide_Returns8For16And2()
+    {
+        var calculator = new Calculator();
+
+        var result = calculator.Divide(16, 2);
+
+        Assert.AreEqual(8, result);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(DivideByZeroException))]
+    public void Calculator_Divide_ThrowsExceptionFor20And0()
+    {
+        var calculator = new Calculator();
+
+        calculator.Divide(20, 0);
+    }
+}
