40328b617f887afdacdc9bc47e2c597c8b9d9656
“saikumar” <pawarsaikumar82@gmail.com>
2024-12-08 18:18:58 +0530

    LinqGrouping

---
+++ LinqGrouping/Program.cs
@@ -0,0 +1,22 @@
+﻿
+var persons = new List<Person>{
+    new Person("Jason", "Lee", "Sales"),
+    new Person("John", "Wick", "Development"),
+    new Person("Ricky", "Pointing", "Development"),
+    new Person("Sandeep", "Sharma", "Sales"),
+    new Person("Ramesh", "Pawar", "Development")
+};
+
+var personsByDepartment = persons.GroupBy(p => p.Department);
+
+foreach (var department in personsByDepartment)
+{
+    Console.WriteLine($"Department: {department.Key}");
+
+    foreach (var employee in department)
+    {
+        Console.WriteLine($" {employee.FirstName} {employee.LastName}");
+    }
+}
+
+public record Person(string FirstName, string LastName, string Department);
