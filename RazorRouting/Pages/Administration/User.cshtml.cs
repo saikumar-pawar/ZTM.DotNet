9ed04cf63a391114967e9ce97529808608c118f3
“saikumar” <pawarsaikumar82@gmail.com>
2024-12-12 23:56:29 +0530

    RazorRouting

---
+++ RazorRouting/Pages/Administration/User.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorRouting.Pages.Administration
+{
+    public class UserModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
