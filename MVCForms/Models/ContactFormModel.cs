ec3980a21d5ee926a80ad8a2daa104439a5757ee
“saikumar” <pawarsaikumar82@gmail.com>
2024-12-21 16:19:14 +0530

    Model Validation

--- MVCForms/Models/ContactFormModel.cs
+++ MVCForms/Models/ContactFormModel.cs
@@ -1,8 +1,18 @@
-﻿namespace MVCForms.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCForms.Models;
 
 public class ContactFormModel
 {
+    [Required]
+    [StringLength(100)]
     public required string Name { get; set; }
+    
+    [Required]
+    [EmailAddress]
     public required string Email { get; set; }
+    
+    [Required]
+    [StringLength(500)]
     public string? Message { get; set; }
 }
