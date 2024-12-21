fa02e874ab60a2aaa8cb6ed2a38b78df415f59dc
“saikumar” <pawarsaikumar82@gmail.com>
2024-12-21 16:02:26 +0530

    Forms

---
+++ MVCForms/Controllers/ContactController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCForms.Models;
+
+namespace MVCForms.Controllers;
+
+public class ContactController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Index(ContactFormModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            // Process the data
+            return RedirectToAction("Success");
+        }
+
+        return View(model);
+    }
+
+    public IActionResult Success()
+    {
+        return View();
+    }
+}
---
+++ MVCForms/Models/ContactFormModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MVCForms.Models;
+
+public class ContactFormModel
+{
+    public required string Name { get; set; }
+    public required string Email { get; set; }
+    public string? Message { get; set; }
+}
