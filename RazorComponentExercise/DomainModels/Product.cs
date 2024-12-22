26531a0fd5ee1a9ac9e0320295c344479a6ed196
“saikumar” <pawarsaikumar82@gmail.com>
2024-12-22 19:08:33 +0530

    Razor Component Exercise

---
+++ RazorComponentExercise/DomainModels/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace RazorComponentExercise.DomainModels;
+
+public class Product
+{
+    private static int availableProductId = 1;
+
+    public Product(string productName, int price)
+    {
+        Id = availableProductId;
+        ProductName = productName;
+        Price = price;
+        availableProductId++;
+    }
+
+    public int Id { get; set; }
+    public string ProductName { get; set; }
+    public int Price { get; set; }
+}
