b1b32dd58827eae578287e3b293d005efec7491c
“saikumar” <pawarsaikumar82@gmail.com>
2024-12-25 18:19:58 +0530

    Using Javascript in Blazor

---
+++ BlazorJavaScriptInterop/wwwroot/js/site.js
@@ -0,0 +1,9 @@
+﻿
+function showMessage(message) {
+    alert(message)
+}
+
+function calculateAge(yearOfBirth) {
+    const currentYear = new Date().getFullYear()
+    return currentYear - yearOfBirth
+}
