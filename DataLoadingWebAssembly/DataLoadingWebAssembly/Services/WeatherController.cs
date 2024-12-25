532baa478cd3179fa8c145033d701f204514a194
“saikumar” <pawarsaikumar82@gmail.com>
2024-12-25 16:01:30 +0530

    DataLoadingWebAssembly

---
+++ DataLoadingWebAssembly/DataLoadingWebAssembly/Services/WeatherController.cs
@@ -0,0 +1,25 @@
+﻿using DataLoadingWebAssembly.Client;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DataLoadingWebAssembly.Services;
+
+[Route("api/[controller]")]
+[ApiController]
+public class WeatherController : ControllerBase
+{
+    public async Task<ActionResult<List<WeatherForecast>>> GetWeatherForecasts()
+    {
+        // Simulate asynchronous loading to demonstrate streaming rendering
+        await Task.Delay(500);
+
+        var startDate = DateOnly.FromDateTime(DateTime.Now);
+        var summaries = new[] { "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" };
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = startDate.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = summaries[Random.Shared.Next(summaries.Length)]
+        }).ToList();
+    }
+}
