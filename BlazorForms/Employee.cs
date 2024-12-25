d74a6f2b58add6a14a943d61817ce56a04cbdf38
“saikumar” <pawarsaikumar82@gmail.com>
2024-12-25 17:10:28 +0530

    Html Forms with Blazor

---
+++ BlazorForms/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorForms;
+
+public class Employee
+{
+    public string? FirstName { get; set; }
+    public string? Lastname { get; set; }
+    public DateOnly DateOfBirth { get; set; }
+    public decimal Salary { get; set; }
+}
