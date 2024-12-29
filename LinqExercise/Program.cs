556ed7215003a3c35aad79b2db5367c9f0e24609
“saikumar” <pawarsaikumar82@gmail.com>
2024-12-29 21:26:17 +0530

    Spelling change in LinqExercise

--- LinqExercise/Program.cs
+++ LinqExercise/Program.cs
@@ -2,7 +2,7 @@
     new Building("Magadha", 1500, 200.6m),
     new Building("Spring", 2600, 435.77m),
     new Building("Attari", 1400, 148.28m),
-    new Building("EMpire State", 12550, 840.32m),
+    new Building("Empire State", 12550, 840.32m),
 };
 
 var totalEstimatedTax = buildings.Sum(b => b.SizeInSqrFt * b.TaxPerSqrFt);
